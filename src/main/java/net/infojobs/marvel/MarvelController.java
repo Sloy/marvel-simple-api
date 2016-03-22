e8d37c9d71ceee7a8e4c245c45925f5b68b3e759
Rafa Vázquez <rafa.vazsan@gmail.com>
2016-03-22 20:36:21 +0100

    Extracted API requests and transforming to a repository

--- src/main/java/net/infojobs/marvel/MarvelController.java
+++ src/main/java/net/infojobs/marvel/MarvelController.java
@@ -2,60 +2,27 @@ package net.infojobs.marvel;
 
 import com.arnaudpiroelle.marvel.api.exceptions.AuthorizationException;
 import com.arnaudpiroelle.marvel.api.exceptions.QueryException;
-import com.arnaudpiroelle.marvel.api.objects.Character;
-import com.arnaudpiroelle.marvel.api.params.name.character.ListCharacterParamName;
-import com.arnaudpiroelle.marvel.api.services.sync.CharactersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Random;
-import java.util.stream.Collectors;
-
-import static java.util.stream.Collectors.toList;
 
 @RestController
 public class MarvelController {
 
     @Autowired
-    private CharactersService charactersService;
-    private Random random = new Random();
+    MarvelRepository marvelRepository;
 
     @RequestMapping("/characters")
     public List<SimpleCharacter> characters() throws IOException, QueryException, AuthorizationException {
-        Map<ListCharacterParamName, String> options = new HashMap<>();
-        options.put(ListCharacterParamName.ORDER_BY, "-modified");
-        options.put(ListCharacterParamName.LIMIT, "100");
-        List<Character> results = charactersService.listCharacter(options).getData().getResults();
-
-        return results.stream()
-          .filter(character -> !character.getDescription().isEmpty())
-          .sorted((o1, o2) -> randomOrder())
-          .map(SimpleCharacter::fromCharacter)
-          .collect(toList());
+        return marvelRepository.characters();
     }
 
     @RequestMapping("/characters/{name}")
     public SimpleCharacter character(@PathVariable("name") String name) throws QueryException, AuthorizationException {
-        Map<ListCharacterParamName, String> options = new HashMap<>();
-        options.put(ListCharacterParamName.NAME, name);
-        List<Character> results = charactersService.listCharacter(options).getData().getResults();
-
-        return results.stream()
-          .map(SimpleCharacter::fromCharacter)
-          .limit(1)
-          .collect(Collectors.collectingAndThen(toList(), l -> {
-              if (l.size() == 1) return l.get(0);
-              throw new RuntimeException();
-          }));
-    }
-
-    private int randomOrder() {
-        return random.nextInt(2) - 1;
+        return marvelRepository.character(name);
     }
 }
---
+++ src/main/java/net/infojobs/marvel/MarvelRepository.java
@@ -0,0 +1,62 @@
+package net.infojobs.marvel;
+
+import com.arnaudpiroelle.marvel.api.exceptions.AuthorizationException;
+import com.arnaudpiroelle.marvel.api.exceptions.QueryException;
+import com.arnaudpiroelle.marvel.api.objects.Character;
+import com.arnaudpiroelle.marvel.api.params.name.character.ListCharacterParamName;
+import com.arnaudpiroelle.marvel.api.services.sync.CharactersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+
+@Repository
+public class MarvelRepository {
+
+    @Autowired
+    private CharactersService charactersService;
+
+    private Random random = new Random();
+
+    public List<SimpleCharacter> characters() throws IOException, QueryException, AuthorizationException {
+        Map<ListCharacterParamName, String> options = new HashMap<>();
+        options.put(ListCharacterParamName.ORDER_BY, "-modified");
+        options.put(ListCharacterParamName.LIMIT, "100");
+        List<Character> results = charactersService.listCharacter(options).getData().getResults();
+
+        return results.stream()
+          .filter(character -> !character.getDescription().isEmpty())
+          .sorted((o1, o2) -> randomOrder())
+          .map(SimpleCharacter::fromCharacter)
+          .collect(toList());
+    }
+
+    public SimpleCharacter character(@PathVariable("name") String name) throws QueryException, AuthorizationException {
+        Map<ListCharacterParamName, String> options = new HashMap<>();
+        options.put(ListCharacterParamName.NAME, name);
+        List<Character> results = charactersService.listCharacter(options).getData().getResults();
+
+        return results.stream()
+          .map(SimpleCharacter::fromCharacter)
+          .limit(1)
+          .collect(Collectors.collectingAndThen(toList(), l -> {
+              if (l.size() == 1) return l.get(0);
+              throw new RuntimeException();
+          }));
+    }
+
+    private int randomOrder() {
+        return random.nextInt(2) - 1;
+    }
+}
