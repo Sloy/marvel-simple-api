b3ee6c581201c371e571421f1db5482c8cff3bd4
Rafa Vázquez <rafa.vazsan@gmail.com>
2016-03-22 17:57:49 +0100

    Base project

---
+++ src/main/java/net/infojobs/marvel/MarvelIjApplication.java
@@ -0,0 +1,12 @@
+package net.infojobs.marvel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarvelIjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarvelIjApplication.class, args);
+	}
+}
